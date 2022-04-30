ad2a1c5595c8a0c5c64fdd8fcfdaee26395d994b
SDL <you@example.com>
2022-04-30 13:29:43 -0700

    V016

--- updateWeb.py
+++ updateWeb.py
@@ -45,36 +45,39 @@ def sendCommandToIP(myIP, myCommand):
 
 
 def update_SafeCast(CPM, uSVh):
-    print("Updating SafeCast")
-    try:
-        measurement = safecast.add_measurement(json={
-        'latitude': updateWebConfig.RADLATITUDE, 
-        'longitude': updateWebConfig.RADLONGITUDE,
-        'value': CPM,
-        'unit': SafecastPy.UNIT_CPM,
-        'captured_at': datetime.datetime.utcnow().isoformat() + '+00:00',
-        'device_id': 90,
-        'location_name': updateWebConfig.RADLOCATIONNAME, 
-        'height': updateWebConfig.RADHEIGHTINMETERS 
-        })
-        print("CPM measurement= ", measurement)
-        print('New CPM measurement id: {0}'.format(measurement['id']))
 
-        measurement = safecast.add_measurement(json={
-        'latitude': updateWebConfig.RADLATITUDE, 
-        'longitude': updateWebConfig.RADLONGITUDE,
-        'value': uSVh,
-        'unit': SafecastPy.UNIT_USV,
-        'captured_at': datetime.datetime.utcnow().isoformat() + '+00:00',
-        'device_id': 90,
-        'location_name': updateWebConfig.RADLOCATIONNAME, 
-        'height': updateWebConfig.RADHEIGHTINMETERS 
-        })
-        print("measurement= ", measurement)
-        print('New measurement id: {0}'.format(measurement['id']))
-    except Exception:
-        traceback.print_exc()
-        return 
+    if (updateWebConfig.SAFECASTAPI != ""):
+
+        print("Updating SafeCast")
+        try:
+            measurement = safecast.add_measurement(json={
+            'latitude': updateWebConfig.RADLATITUDE, 
+            'longitude': updateWebConfig.RADLONGITUDE,
+            'value': CPM,
+            'unit': SafecastPy.UNIT_CPM,
+            'captured_at': datetime.datetime.utcnow().isoformat() + '+00:00',
+            'device_id': 90,
+            'location_name': updateWebConfig.RADLOCATIONNAME, 
+            'height': updateWebConfig.RADHEIGHTINMETERS 
+            })
+            print("CPM measurement= ", measurement)
+            print('New CPM measurement id: {0}'.format(measurement['id']))
+    
+            measurement = safecast.add_measurement(json={
+            'latitude': updateWebConfig.RADLATITUDE, 
+            'longitude': updateWebConfig.RADLONGITUDE,
+            'value': uSVh,
+            'unit': SafecastPy.UNIT_USV,
+            'captured_at': datetime.datetime.utcnow().isoformat() + '+00:00',
+            'device_id': 90,
+            'location_name': updateWebConfig.RADLOCATIONNAME, 
+            'height': updateWebConfig.RADHEIGHTINMETERS 
+            })
+            print("measurement= ", measurement)
+            print('New measurement id: {0}'.format(measurement['id']))
+        except Exception:
+            traceback.print_exc()
+            return 
  
     pass
 
@@ -84,21 +87,23 @@ def update_NETC(CPM):
     pass
 
 def update_RadMon(CPM):
-    
-    myCommand = "radmon.php?function=submit&user="+updateWebConfig.RADMONUSER +"&password="+updateWebConfig.RADMONPASSWORD+"&value="+str(CPM)+ "&unit=CPM" 
-    print("myCommand=", myCommand)
-    response = sendCommandToIP(updateWebConfig.RADMONIPADDRESS, myCommand) 
-    print("response=", response)
-    pass
+   
+    if (updateWebConfig.RADMONUSER != ""):
+        myCommand = "radmon.php?function=submit&user="+updateWebConfig.RADMONUSER +"&password="+updateWebConfig.RADMONPASSWORD+"&value="+str(CPM)+ "&unit=CPM" 
+        print("myCommand=", myCommand)
+        response = sendCommandToIP(updateWebConfig.RADMONIPADDRESS, myCommand) 
+        print("response=", response)
+        pass
 
 def update_GMCMap(CPM, uSVh):
    
 
+    if (updateWebConfig.GMCMAPUSERACCOUNTID != ""):
 
-    myCommand = "log2.asp?AID="+updateWebConfig.GMCMAPUSERACCOUNTID+"&GID="+updateWebConfig.GMCMAPGEIGERCOUNTERID+"&CPM="+str(CPM)+"&uSV="+str(uSVh)
-    print("myCommand=", myCommand)
-    response = sendCommandToIP(updateWebConfig.GMCMAPIPADDRESS, myCommand) 
-    print("response=", response)
-
+        myCommand = "log2.asp?AID="+updateWebConfig.GMCMAPUSERACCOUNTID+"&GID="+updateWebConfig.GMCMAPGEIGERCOUNTERID+"&CPM="+str(CPM)+"&uSV="+str(uSVh)
+        print("myCommand=", myCommand)
+        response = sendCommandToIP(updateWebConfig.GMCMAPIPADDRESS, myCommand) 
+        print("response=", response)
+    
 
     pass
